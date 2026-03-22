3096273e1c98d470ca5049cc9cb042252c50c390
Ruvinda24 <shalukaofficial24@gmail.com>
2026-03-22 15:13:30 +0530

    chore: The console error Cross-Origin-Opener-Policy policy would block the window.postMessage call happens because Vite's default local development server adds strict security headers that prevent Google's popup window from communicating back to your main window. to fix this i added server bloc in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,10 @@ export default defineConfig({
         react(),
         tailwindcss(),
     ],
+    server: {
+        port: 5173,
+        headers: {
+            'Cross-Origin-Opener-Policy': 'same-origin-allow-popups'
+        }
+    }
 })
